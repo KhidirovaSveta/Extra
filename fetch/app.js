a51d5189ee77d6f7e9c4b5fdb3ea53d3e47c50ab
KhidirovaSveta <khidirova.sveta@gmail.com>
2022-12-13 09:51:21 +0400

    updated

--- fetch/app.js
+++ fetch/app.js
@@ -1,57 +1,20 @@
-// const BASE_URL_2 = "https://api.thecatapi.com/v1/";
-// let search = document.querySelector("#searchHere");
-// let box = document.querySelector(".box");
-
-// function fetchData(endpoint) {
-//   fetch(`${BASE_URL_2}${endpoint}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       function searchCat(catSearch) {
-//         for (let i = 0; i < catSearch.length; i++) {
-//           box.innerHTML += `<div class="card" style="width: 18rem;">
-//           <img src="${catSearch[i].image.url}" class="card-img-top" alt="...">
-//           <div class="card-body">
-//           <h5 class="card-title">${catSearch[i].name}</h5>
-//           <p class="card-text"> ${catSearch[i].description}</p>
-//           <a target = "_blank" href="${catSearch[i].wikipedia_url}" class="btn btn-primary">Go somewhere</a>
-//           </div>
-//           </div>`;
-//         }
-//       }
-//       searchCat(data);
-
-//       search.addEventListener("input", function () {
-//         let filteredData = data.filter((data) =>
-//           data.name
-//             .toLocaleLowerCase()
-//             .includes(search.value.toLocaleLowerCase())
-//         );
-//         box.innerHTML = "";
-//         searchCat(filteredData);
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// }
-
-// fetchData("breeds");
-
 const BASE_URL_2 = "https://api.thecatapi.com/v1/";
 let search = document.querySelector("#search");
-let box = document.querySelector(".box");
+let box = document.querySelector("#cat");
 
 function fetchData(endpoint) {
   fetch(`${BASE_URL_2}${endpoint}`)
     .then((response) => response.json())
     .then((data) => {
-      function searchCat(array) {
-        for (let i = 0; i < array.length; i++) {
+      function searchCat(catSearch) {
+        for (let i = 0; i < catSearch.length; i++) {
           box.innerHTML += `<div class="card" style="width: 18rem;">
-            <img src="${array[i].image?.url}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${array[i].name}</h5>
-              <p class="card-text"> ${array[i].description}</p>
-              <a target = "_blank" href="${array[i].wikipedia_url}" class="btn btn-primary">Go somewhere</a>
-            </div>
+          <img src="${catSearch[i].image.url}" class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="card-title">${catSearch[i].name}</h5>
+          <p class="card-text"> ${catSearch[i].description}</p>
+          <a target = "_blank" href="${catSearch[i].wikipedia_url}" class="btn btn-primary">Go somewhere</a>
+          </div>
           </div>`;
         }
       }
@@ -63,14 +26,51 @@ function fetchData(endpoint) {
             .toLocaleLowerCase()
             .includes(search.value.toLocaleLowerCase())
         );
-        // console.log(filteredData);
         box.innerHTML = "";
         searchCat(filteredData);
       });
     })
     .catch((err) => console.log(err));
 }
+
 fetchData("breeds");
+
+// const BASE_URL_2 = "https://api.thecatapi.com/v1/";
+// let search = document.querySelector("#search");
+// let box = document.querySelector(".box");
+
+// function fetchData(endpoint) {
+//   fetch(`${BASE_URL_2}${endpoint}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       function searchCat(array) {
+//         for (let i = 0; i < array.length; i++) {
+//           box.innerHTML += `<div class="card" style="width: 18rem;">
+//             <img src="${array[i].image?.url}" class="card-img-top" alt="...">
+//             <div class="card-body">
+//               <h5 class="card-title">${array[i].name}</h5>
+//               <p class="card-text"> ${array[i].description}</p>
+//               <a target = "_blank" href="${array[i].wikipedia_url}" class="btn btn-primary">Go somewhere</a>
+//             </div>
+//           </div>`;
+//         }
+//       }
+//       searchCat(data);
+
+//       search.addEventListener("input", function () {
+//         let filteredData = data.filter((data) =>
+//           data.name
+//             .toLocaleLowerCase()
+//             .includes(search.value.toLocaleLowerCase())
+//         );
+//         // console.log(filteredData);
+//         box.innerHTML = "";
+//         searchCat(filteredData);
+//       });
+//     })
+//     .catch((err) => console.log(err));
+// }
+// fetchData("breeds");
 
 // const BASE_URL = "https://jsonplaceholder.typicode.com/";
 // let tbody = document.querySelector("tbody");
